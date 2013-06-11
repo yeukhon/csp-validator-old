4d4217b4a60a94133cff96bb55e43bb866ccc6a6
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-06-11 16:54:44 -0700

    Started putting the rest of the grammar in.
    Unable to parse the second+ item in the sourcenlist.

--- p.py
+++ p.py
@@ -1,9 +1,18 @@
 import pyparsing
-from pyparsing import alphas, Literal, Word, ZeroOrMore, Regex
+from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore
 
 directive_name = Literal("default-src") | Literal("script-src")
-directive_value = Literal("'self'") | Literal("'none'") | Literal("https:")
-#directive = ZeroOrMore(Regex(' \t')) + directive_name + Regex(' \t') + directive_value
+#directive_value = Literal("'self'") | Literal("'none'") | Literal("https:")
+keyword_source = Literal("'self'") | Literal("'unsafe-inline'") | Literal("'unsafe-eval'") | Literal("'none'")
+port = ":" + nums
+scheme = Literal("http") | Literal("https")
+host_char = Word(alphas) | Word(nums) | Literal("-")
+host = Literal("*") | Literal("*.") + host_char + ZeroOrMore("." + host_char)
+host_source = Optional(scheme + "://") + host + Optional(port)
+source_expression = host_source | keyword_source # skip scheme_source for now
+#source_list = source_expression + ZeroOrMore(OneOrMore(Regex(' \t')) + source_expression)
+source_list = source_expression + ZeroOrMore(OneOrMore(Literal(" "))+ source_expression)
+directive_value = source_list
 directive = directive_name + directive_value
 remain_policy = ';' + directive
 policy = directive + ZeroOrMore(remain_policy)
@@ -13,9 +22,8 @@ strs = [
         "default-src 'self';",
         "default-src 'none'",
         "default-src 'none';",
-        "default-src https:",
-        "default-src https:;",
-        "default-src none"
+        "default-src 'self' trusted-scripts.foo.com",
+        "default-src 'self' 'none';"
 ]
 
 def test():
@@ -24,7 +32,8 @@ def test():
         try:
             print str, ' ----> ', policy.parseString(str)
         except pyparsing.ParseException:
-            pass
+            print "ERROR"
+            
     print '++'
 
     for str in strs:
@@ -32,6 +41,7 @@ def test():
             str = str.replace('default-src', 'script-src')
             print str, ' ---->', policy.parseString(str)
         except pyparsing.ParseException:
-            pass
+            print "ERROR"
+
 
 test()        
