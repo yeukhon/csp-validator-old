a486a38c969b0c468679e47e8f7c9a8cecf8c575
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-06-12 11:09:52 -0700

    Now scanner will actual read urls coming from 2+ position.

--- p.py
+++ p.py
@@ -1,16 +1,16 @@
 import pyparsing
-from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore, Group, Dict, Suppress, Or
+from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore, Group, Dict, Suppress, Or, delimitedList, StringEnd, printables, Combine, alphanums
 
 
 space = Suppress(White(" \t"))
 port = Word(nums)
-host_char = Word(alphas) | Word(nums) | Literal("-") | Literal(".")
-#host = Literal("*") | Literal("*.") + OneOrMore(host_char + ZeroOrMore(Literal("*") + OneOrMore(host_char)))
-host = OneOrMore(host_char) 
-scheme = Literal("http") | Literal("https")
-host_source = Or( scheme + Literal("://") | Literal("*.")) + host + Optional(port)
+host_char = Word(alphas + nums + "-" + ".")
+#host_char = Regex(r'[a-zA-Z][a-zA-Z0-9+.-]*')   # rfc 3986 implementation
+host = Literal("*") | OneOrMore(host_char + ZeroOrMore(Literal("*") + OneOrMore(host_char)))
+scheme = Literal("https") | Literal("http")
+host_source = Optional(scheme + Literal("://")) + Optional(Literal("*.")) + host + Optional(port)
 keyword_source = Literal("'self'") | Literal("'unsafe-inline'") | Literal("'unsafe-eval'") | Literal("'none'")
-source_expression = host_source | keyword_source
+source_expression = delimitedList(host_source, combine=True, delim=Literal(".")) | keyword_source
 source_list = ZeroOrMore(space) + source_expression + ZeroOrMore(OneOrMore(space) + source_expression) + ZeroOrMore(space)
 directive_value = source_list
 directive_name = Literal("default-src") | Literal("script-src")
@@ -19,18 +19,18 @@ rest_of_directives = Literal(";") + directive
 policy = directive + ZeroOrMore(rest_of_directives)
 
 strs = [
-        "default-src 'self'",
-        "default-src 'self';",
-        "default-src 'none'",
-        "default-src 'none';",
-        "default-src 'self' http://example.org",
-        "default-src 'self' example.org",
-        "default-src 'self' *.example.org",
-        "default-src 'self' 'none';",
-        "default-src http://example.org 'self'",
-        "default-src example.org 'self'",
-        "default-src *.example.org 'self'",
-
+    "default-src 'self'",
+    "default-src 'self';",
+    "default-src 'none'",
+    "default-src 'none';",
+    "default-src 'self' http://example.org",
+    "default-src 'self' example.org",
+    "default-src 'self' *.example.org",
+    "default-src 'self' 'none';",
+    "default-src http://example.org 'self'",
+    "default-src example.org 'self'",
+    "default-src *.example.org 'self'",
+    "default-src example.org http://example.org https://example.org 'none'"
 ]
 
 def test():
