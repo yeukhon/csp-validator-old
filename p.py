26a3f6aa574778fb9203cd16c15234058ec71da0
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-06-11 21:14:14 -0700

    Experimenting with space.

--- p.py
+++ p.py
@@ -1,28 +1,29 @@
 import pyparsing
 from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore
 
+space = White(' \t')
 directive_name = Literal("default-src") | Literal("script-src")
-#directive_value = Literal("'self'") | Literal("'none'") | Literal("https:")
 keyword_source = Literal("'self'") | Literal("'unsafe-inline'") | Literal("'unsafe-eval'") | Literal("'none'")
 port = ":" + nums
 scheme = Literal("http") | Literal("https")
-host_char = Word(alphas) | Word(nums) | Literal("-")
-host = Literal("*") | Literal("*.") + host_char + ZeroOrMore("." + host_char)
+#host_char = Word(alphas) | Word(nums) | Literal("-")
+host_char = Literal("*.foo.com")
+host = Literal("*") | Literal("*.") + OneOrMore(host_char) + ZeroOrMore("." + OneOrMore(host_char))
 host_source = Optional(scheme + "://") + host + Optional(port)
 source_expression = host_source | keyword_source # skip scheme_source for now
-#source_list = source_expression + ZeroOrMore(OneOrMore(Regex(' \t')) + source_expression)
-source_list = source_expression + ZeroOrMore(OneOrMore(Literal(" "))+ source_expression)
-directive_value = source_list
-directive = directive_name + directive_value
+source_list = ZeroOrMore(space) + source_expression + ZeroOrMore(OneOrMore(space)+ source_expression) + ZeroOrMore(space)
+directive_value = ZeroOrMore(space | source_list)
+directive = ZeroOrMore(space) + directive_name + space + directive_value
 remain_policy = ';' + directive
 policy = directive + ZeroOrMore(remain_policy)
+print policy
 
 strs = [
         "default-src 'self'",
         "default-src 'self';",
         "default-src 'none'",
         "default-src 'none';",
-        "default-src 'self' trusted-scripts.foo.com",
+        "default-src 'self' *.foo.com",
         "default-src 'self' 'none';"
 ]
 
