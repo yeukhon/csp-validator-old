55e16ec8bd9728e413dd72134ebe14b0a1a7cefc
Yeuk Hon Wong <yeukhon@mozilla.com>
2013-06-11 21:29:31 -0700

    Started rewriting all the rules; can read second item.

--- p.py
+++ p.py
@@ -1,23 +1,13 @@
 import pyparsing
-from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore
+from pyparsing import alphas, Literal, nums, OneOrMore, Optional, Regex, White, Word, ZeroOrMore, Group, Dict, Suppress
 
-space = White(' \t')
+
+space = Suppress(White(" \t"))
+directive_value = Literal("'self'") | Literal("'none'") | Literal("*.foo.com")
 directive_name = Literal("default-src") | Literal("script-src")
-keyword_source = Literal("'self'") | Literal("'unsafe-inline'") | Literal("'unsafe-eval'") | Literal("'none'")
-port = ":" + nums
-scheme = Literal("http") | Literal("https")
-#host_char = Word(alphas) | Word(nums) | Literal("-")
-host_char = Literal("*.foo.com")
-host = Literal("*") | Literal("*.") + OneOrMore(host_char) + ZeroOrMore("." + OneOrMore(host_char))
-host_source = Optional(scheme + "://") + host + Optional(port)
-source_expression = host_source | keyword_source # skip scheme_source for now
-source_list = ZeroOrMore(space) + source_expression + ZeroOrMore(OneOrMore(space)+ source_expression) + ZeroOrMore(space)
-directive_value = ZeroOrMore(space | source_list)
-directive = ZeroOrMore(space) + directive_name + space + directive_value
-remain_policy = ';' + directive
-policy = directive + ZeroOrMore(remain_policy)
-print policy
-
+directive = Dict(Group(directive_name + space + OneOrMore(directive_value + ZeroOrMore(space))))
+rest_of_directives = Literal(";") + directive
+policy = directive + ZeroOrMore(rest_of_directives)
 strs = [
         "default-src 'self'",
         "default-src 'self';",
@@ -31,6 +21,8 @@ def test():
 
     for str in strs:
         try:
+            p = policy.parseString(str)
+            print 'v ', p['default-src']
             print str, ' ----> ', policy.parseString(str)
         except pyparsing.ParseException:
             print "ERROR"
